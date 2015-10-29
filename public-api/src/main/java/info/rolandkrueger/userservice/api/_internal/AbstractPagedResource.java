e9b0f5d1849333d0ae107682e2adccf31672050a
Roland Krüger <info@rolandkrueger.info>
2015-10-29 20:15:39 +0100

    Add JavaDoc

--- public-api/src/main/java/info/rolandkrueger/userservice/api/_internal/AbstractPagedResource.java
+++ public-api/src/main/java/info/rolandkrueger/userservice/api/_internal/AbstractPagedResource.java
@@ -18,14 +18,23 @@ import java.util.HashMap;
 
 /**
  * A REST collection resource that allows to page through the list of entities. This class adds methods to the general
- * {@link AbstractResource} interface which support paging and sorting the collection resource's data.
+ * {@link AbstractResource} interface supporting paging and sorting the collection resource's data.
+ * <p/>
+ * When you create a sub-class for this paged resource then parameter T denotes the data type represented by the resource,
+ * and parameter R is the subclass itself. E. g., if you have a domain type <code>Person</code> and a corresponding
+ * client-side data type <code>PersonApiData</code> then a paged <code>Person</code> paged resource could have the following
+ * signature:
+ * <p/>
+ * <code>
+ * public class PersonsResource extends AbstractPagedResource&lt;PersonApiData, PersonPagedResource&gt; {...}
+ * </code>
  *
  * @param <T> type of the data object represented by this collection resource
+ * @param <R> type of the sub-class inheriting from this resource
  * @author Roland Krüger
  * @see AbstractBaseApiData
  */
 public abstract class AbstractPagedResource<T extends AbstractBaseApiData<?>, R extends AbstractPagedResource> extends AbstractResource<T> {
-
     /**
      * Response entity object for this collection resource.
      */
@@ -38,35 +47,104 @@ public abstract class AbstractPagedResource<T extends AbstractBaseApiData<?>, R 
         super(templatedSelfLink, self);
     }
 
+    /**
+     * Factory method for creating a new instance of this resource. Has to be implemented by sub-classes in such a way
+     * that this implementation returns a new instance of the implementing class for the given self link.
+     *
+     * @param self link to self to be used by the created resource object
+     * @return a new instance of the implementing sub-class
+     */
     protected abstract R createResourceInstance(Link self);
 
+    /**
+     * Sets the templated link to self for this resource.
+     */
     protected final void setTemplatedSelfLink(Link templatedSelfLink) {
         this.templatedSelfLink = templatedSelfLink;
     }
 
-    public final R goToPageSorted(Integer page, Integer size, String sortBy, SortDirection direction) {
-        R resource = createResourceInstance(expandLink(templatedSelfLink, page, size, sortBy, direction));
+    /**
+     * Configures this resource to return its data as a sorted list for a given page with a given size. This method
+     * returns a new paged resource object with its self link parameterized to sort by the specified property and to
+     * return the specified page. This method will only create a parameterized resource object and will not yet call
+     * the service to fetch the data.
+     *
+     * @param page           requested page number
+     * @param size           requested page size
+     * @param sortByProperty sort property, has to be a valid property name of the resource's data type
+     * @param direction      sort direction: ascending or descending
+     * @return a new paged resource object with its self link parameterized to sort by the specified parameters limited by
+     * the specified page
+     */
+    public final R goToPageSorted(Integer page, Integer size, String sortByProperty, SortDirection direction) {
+        R resource = createResourceInstance(expandLink(templatedSelfLink, page, size, sortByProperty, direction));
         resource.setTemplatedSelfLink(templatedSelfLink);
         return resource;
     }
 
-    public final R sort(String sortBy, SortDirection direction) {
-        R resource = createResourceInstance(expandLink(templatedSelfLink, null, null, sortBy, direction));
+    /**
+     * Configures this resource to return its data as a sorted list. This method returns a new paged resource object
+     * with its self link parameterized to sort by the specified property. This method will only create a parameterized
+     * resource object and will not yet call the service to fetch the data.
+     * <p/>
+     * Note that this method will retain any paging parameters already set with {@link #goToPage(Integer, Integer)}.
+     * I. e., the following two statements are equivalent:
+     * <code>
+     * pagedResource.goToPage(5, 20).sort("name", SortDirection.ASC);
+     * pageResource.goToPageSorted(5, 20, "name", SortDirection.ASC);
+     * </code>
+     *
+     * @param sortByProperty sort property, has to be a valid property name of the resource's data type
+     * @param direction      sort direction: ascending or descending
+     * @return a new paged resource object with its self link parameterized to sort by the specified parameters
+     */
+    public final R sort(String sortByProperty, SortDirection direction) {
+        R resource = createResourceInstance(expandLink(templatedSelfLink, null, null, sortByProperty, direction));
         resource.setTemplatedSelfLink(templatedSelfLink);
         return resource;
     }
 
+    /**
+     * Configures this resource to go to a particular page on the resource data list.
+     * <p/>
+     * Note that this method will retain any sorting parameters already set with {@link #sort(String, SortDirection)}.
+     * I. e., the following two statements are equivalent:
+     * <code>
+     * pagedResource.goToPage(5, 20).sort("name", SortDirection.ASC);
+     * pageResource.goToPageSorted(5, 20, "name", SortDirection.ASC);
+     * </code>
+     *
+     * @param page requested page number
+     * @param size requested page size
+     * @return a new paged resource object with its self link parameterized to provide a particular page
+     */
     public final R goToPage(Integer page, Integer size) {
         R resource = createResourceInstance(expandLink(templatedSelfLink, page, size, null, null));
         resource.setTemplatedSelfLink(templatedSelfLink);
         return resource;
     }
 
+    /**
+     * Checks if there is a next page available for the current page.
+     * <p/>
+     * This method will call the service (if not already done) to read this information from the service response.
+     *
+     * @return <code>true</code> if {@link #next()} can be called
+     * @throws RestClientException when an error occurred while communicating with the service
+     */
     public final boolean hasNext() throws RestClientException {
         loadIfNecessary();
         return responseEntity.getBody().getNextLink() != null;
     }
 
+    /**
+     * Returns the next page for the list of resource data objects. Prior to calling this method, it should be
+     * checked with {@link #hasNext()} if there is a next page available at all.
+     *
+     * @return a new paged resource object with its page parameters configured to provide the next page
+     * @throws RestClientException   when an error occurred while communicating with the service
+     * @throws IllegalStateException if there is no next page available
+     */
     public final R next() throws RestClientException {
         R resource = createResourceInstance(nextPageLink());
         resource.setTemplatedSelfLink(templatedSelfLink);
@@ -74,8 +152,7 @@ public abstract class AbstractPagedResource<T extends AbstractBaseApiData<?>, R 
     }
 
     /**
-     * Returns the link to the next page of this paged resource. Before this method is called, it should be checked
-     * whether there is a next page available with {@link #hasNext()}.
+     * Returns the link to the next page of this paged resource.
      *
      * @throws RestClientException   when an error occurred while communicating with the service
      * @throws IllegalStateException if this resource points to the last page of the collection resource
@@ -86,11 +163,27 @@ public abstract class AbstractPagedResource<T extends AbstractBaseApiData<?>, R 
         return responseEntity.getBody().getNextLink();
     }
 
+    /**
+     * Checks if there is a previous page available for the current page.
+     * <p/>
+     * This method will call the service (if not already done) to read this information from the service response.
+     *
+     * @return <code>true</code> if {@link #previous()} can be called
+     * @throws RestClientException when an error occurred while communicating with the service
+     */
     public final boolean hasPrevious() throws RestClientException {
         loadIfNecessary();
         return responseEntity.getBody().getPreviousLink() != null;
     }
 
+    /**
+     * Returns the previous page for the list of resource data objects. Prior to calling this method, it should be
+     * checked with {@link #hasPrevious()} if there is a previous page available at all.
+     *
+     * @return a new paged resource object with its page parameters configured to provide the previous page
+     * @throws RestClientException   when an error occurred while communicating with the service
+     * @throws IllegalStateException if there is no previous page available
+     */
     public final R previous() throws RestClientException {
         R resource = createResourceInstance(previousPageLink());
         resource.setTemplatedSelfLink(templatedSelfLink);
@@ -98,8 +191,7 @@ public abstract class AbstractPagedResource<T extends AbstractBaseApiData<?>, R 
     }
 
     /**
-     * Returns the link to the previous page of this paged resource. Before this method is called, it should be checked
-     * whether there is a previous page available with {@link #hasPrevious()}.
+     * Returns the link to the previous page of this paged resource.
      *
      * @throws RestClientException   when an error occurred while communicating with the service
      * @throws IllegalStateException if this resource points to the first page of the collection resource
@@ -174,7 +266,9 @@ public abstract class AbstractPagedResource<T extends AbstractBaseApiData<?>, R 
      * @param sortBy    sort property (may be <code>null</code>)
      * @param direction sort direction (ascending or descending; may be <code>null</code> and will be ignored is sortBy
      *                  is <code>null</code>). If <code>null</code>, ascending sort direction will be used by default.
-     * @return
+     * @return an expanded link object whose template parameters are configured with the given values. This resulting
+     * link may be templated itself.
+     * @throws IllegalArgumentException if the provided link is not templated
      */
     protected final Link expandLink(final Link link, final Integer page, final Integer size, final String sortBy, final
     SortDirection direction) {
