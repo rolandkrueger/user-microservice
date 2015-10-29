5867c8014d1e93b1c5b6894e76c072e641963e65
Roland Krüger <info@rolandkrueger.info>
2015-10-29 20:00:38 +0100

    Move web security configuration into its own class

--- service/src/main/java/info/rolandkrueger/userservice/UserMicroserviceApplication.java
+++ service/src/main/java/info/rolandkrueger/userservice/UserMicroserviceApplication.java
@@ -4,30 +4,16 @@ import info.rolandkrueger.userservice.application.DevelopmentProfileConfiguratio
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@EnableWebSecurity
 @PropertySource("userservice.properties")
-public class UserMicroserviceApplication extends WebSecurityConfigurerAdapter {
+public class UserMicroserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(
-                new Object[] {
+                new Object[]{
                         UserMicroserviceApplication.class,
-                        DevelopmentProfileConfiguration.class }
+                        DevelopmentProfileConfiguration.class}
                 , args);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll();
-
-        http.csrf().disable();
     }
 }
---
+++ service/src/main/java/info/rolandkrueger/userservice/application/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package info.rolandkrueger.userservice.application;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Roland Krüger
+ */
+@EnableWebSecurity
+@Component
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+
+        http.csrf().disable();
+    }
+
+}
